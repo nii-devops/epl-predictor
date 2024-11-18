31055820d78b586a9a58297261bc812c35416791
Nii Ako Adjei <niiakoadjei.devops@gmail.com>
2024-11-18 16:40:35 +0000

    add aws db uri

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DB_URI')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('AWS_DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
