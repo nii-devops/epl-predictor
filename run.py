c2560ac5c986a019a05c1d0ef3130077af893996
Nii Ako Adjei <niiakoadjei.devops@gmail.com>
2024-11-19 22:17:24 +0000

    change port

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from app import app, db
 
 
 
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()  # Create tables if they don't exist
-#     app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables if they don't exist
-    app.run(port=10000,debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()  # Create tables if they don't exist
+#     app.run(port=10000,debug=True)
